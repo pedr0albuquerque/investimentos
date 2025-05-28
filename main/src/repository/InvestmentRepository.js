75adb3f4c7ae291c2b47261326c5d3a1ffc9efa4
Pedro Vieira <pedroalbuquerque4455@gmail.com>
2025-05-28 15:16:04 -0300

    Controller funcionando

--- main/src/repository/InvestmentRepository.js
+++ main/src/repository/InvestmentRepository.js
@@ -1,18 +1,60 @@
 import { Investment } from "../model/Investiment.js";
 
 async function createInvestment(nameInvestment, typeInvestment, valueInvestment, dateInvestment) {
+    const investment = await Investment.create({
+        nameInvestment,
+        typeInvestment,
+        valueInvestment,
+        dateInvestment
+    });
+    return investment;
+}
+
+async function getAllInvestments() {
     try {
-        const investment = await Investment.create({
-            nameInvestment,
-            typeInvestment,
-            valueInvestment,
-            dateInvestment
-        });
-        return investment;
+        const investments = await Investment.findAll();
+        return investments;
     } catch (error) {
-        console.error("Error creating investment:", error);
+        console.error("Não foi possível listar os investimentos", error);
         throw error;
     }
 }
 
-console.log(createInvestment('abc123', 'abc123', 1000, '2023-10-01'));
+async function updateInvestment(id, nameInvestment, typeInvestment, valueInvestment, dateInvestment) {
+    try {
+        const investment = await Investment.findByPk(id);
+        if (!investment) {
+            throw new Error("Investment not found");
+        }
+        investment.nameInvestment = nameInvestment;
+        investment.typeInvestment = typeInvestment;
+        investment.valueInvestment = valueInvestment;
+        investment.dateInvestment = dateInvestment;
+        await investment.save();
+        return investment;
+    } catch (error) {
+        console.error("Error updating investment:", error);
+        throw error;
+    }
+}
+
+async function deleteInvestment(id){
+    try {
+        const investment = await Investment.findByPk(id);
+        if (!investment) {
+            throw new Error("Investment not found");
+        }
+        await investment.destroy();
+        return { message: "Investment deleted successfully" };
+    } catch (error) {
+        console.error("Error deleting investment:", error);
+        throw error;
+    }
+}
+
+export {
+    createInvestment,
+    getAllInvestments,
+    updateInvestment,
+    deleteInvestment
+};
